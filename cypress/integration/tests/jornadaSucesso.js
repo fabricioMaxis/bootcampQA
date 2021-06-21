8108a21be64570249805d1fe0644cda7edd5b88f
Fabricio Luiz de Souza Pereira <fabricio.pereira@animaeducacao.com.br>
2021-06-21 13:03:26 -0300

    Jornada com sucesso

---
+++ cypress/integration/tests/jornadaSucesso.js
@@ -0,0 +1,43 @@
+/// <reference types="cypress" />
+
+//Fabricio
+import Login from '../../support/pages/JornadaPage';
+
+describe('hometests', () => {
+  beforeEach(()=>{
+    
+    cy.visit('https://saucedemo.com');
+    
+      
+  })
+
+  afterEach(()=>{
+
+  })
+
+  it('JornadaSucesso', () =>{
+
+    Login.login();
+    Login.clicarLogin();
+    Login.clicarAddCartBackpack();
+    Login.clicarAddCartBike();
+    Login.validarQtdItemCarrinho();
+    Login.clicarCarrinhoButton();
+    Login.validarItemBackpackNoCarrinho();
+    Login.validarItemBikeNoCarrinho();
+    Login.clicarCheckoutButton();
+    Login.checkoutForms();
+    Login.clicarContinue();
+    Login.clicarFinalizar();
+    Login.validarCompraSucesso();
+    Login.validarCompraSucesso();
+  })
+  
+  
+
+    
+   
+
+  
+    
+});
---
+++ cypress/support/pages/JornadaPage/index.js
@@ -0,0 +1,125 @@
+//**********Mapeamento de elementos**********
+const el = {
+  //Campo de texto
+  tfNome: '[data-test=username]', 
+  tfFirstName: '[data-test=firstName]', 
+  tfLastName: '[data-test=lastName]',
+  tfZipCode: '[data-test=postalCode]',  
+  tfPassword: '[data-test=password]',
+  QtdItem:'.shopping_cart_badge',
+  
+  
+  //Botoes
+  btLogin:'[data-test=login-button]',
+  btAddCartBackpack:'[data-test=add-to-cart-sauce-labs-backpack]',
+  btAddCartBike:'[data-test=add-to-cart-sauce-labs-bike-light]',
+  btCarrinho:'.shopping_cart_link',
+  btCheckout:'[data-test=checkout]',
+  btContinue:'[data-test=continue]',
+  btFinalizar:'[data-test=finish]',
+
+
+
+//textos
+txtBackpack:'#item_4_title_link > .inventory_item_name',
+txtBike:'#item_0_title_link > .inventory_item_name',
+txtInfoPagamento:'.summary_info > :nth-child(1)',
+txtInfoEnvio:'.summary_info > :nth-child(3)',
+txtTotalValor:'summary_total_label',
+txtSucesso:'.complete-header',
+txtEnvioSucesso:'.complete-text',
+
+
+
+
+
+}
+
+//**********Ações**********
+class Login {
+
+login(){
+  cy.get(el.tfNome).type('standard_user')
+  cy.get(el.tfPassword).type('secret_sauce')
+
+}
+ clicarLogin(){
+
+  cy.get(el.btLogin).click({force:true});
+
+ }
+ clicarAddCartBackpack(){
+
+  cy.get(el.btAddCartBackpack).click({force:true});
+
+ }
+ clicarAddCartBike(){
+
+  cy.get(el.btAddCartBike).click({force:true});
+
+ }
+ clicarCarrinhoButton(){
+
+  cy.get(el.btCarrinho).click({force:true});
+
+ }
+ validarQtdItemCarrinho(){
+
+  cy.get(el.QtdItem);
+ }
+
+ validarItemBackpackNoCarrinho(){
+
+  cy.get(el.txtBackpack);
+
+ }
+ validarItemBikeNoCarrinho(){
+  cy.get(el.txtBike);
+
+ }
+ clicarCheckoutButton(){
+
+  cy.get(el.btCheckout).click({force:true});
+
+ }
+ checkoutForms(){
+  cy.get(el.tfFirstName).type('Maxis')
+  cy.get(el.tfLastName).type('Crow')
+  cy.get(el.tfZipCode).type('33400000')
+
+}
+clicarContinue(){
+
+  cy.get(el.btContinue).click({force:true});
+
+ }
+ validarInfoPagamento(){
+
+  cy.get(el.txtInfoPagamento);
+ }
+ validarInfoEnvio(){
+
+  cy.get(el.txtInfoEnvio);
+ }
+ validarTotalValor(){
+
+  cy.get(el.txtTotalValor);
+ }
+ clicarFinalizar(){
+
+  cy.get(el.btFinalizar).click({force:true});
+
+ }
+ validarCompraSucesso(){
+
+  cy.get(el.txtSucesso);
+ }
+ validarCompraEnvio(){
+
+  cy.get(el.txtEnvioSucesso);
+ }
+
+
+}
+
+export default new Login();
